dd1dc39059e4bb152715336dda3969ca663612fe
calint <calin.tenitchi@gmail.com>
2026-01-26 00:10:11 +0100

    .

--- src/osca.cpp
+++ src/osca.cpp
@@ -40,12 +40,7 @@ void print_string(u32 x, u32 y, u32 color, char const* str) {
     }
 }
 
-extern "C" auto osca_on_timer() -> void {
-    serial_print(".");
-
-    // acknowledge interrupt
-    *reinterpret_cast<volatile u32*>(0xFEE000B0) = 0;
-}
+extern "C" auto osca_on_timer() -> void { serial_print("."); }
 
 extern "C" auto osca_on_keyboard(u8 scancode) -> void {
     serial_print("kbd: 0x");
