4a7cf32ed72f4683d852b52c9608d3ba92391179
calint <calin.tenitchi@gmail.com>
2026-02-01 18:21:00 +0100

    .

--- src/osca.cpp
+++ src/osca.cpp
@@ -1,5 +1,5 @@
+#include "osca.hpp"
 #include "ascii_font_8x8.hpp"
-#include "kernel.hpp"
 
 namespace {
 
@@ -86,55 +86,6 @@ auto test_simd_support() -> void {
 
 namespace osca {
 
-auto constexpr JOB_QUEUE_LEN = 256u;
-
-class JobQueue final {
-  public:
-    using Job = auto (*)(void*) -> void;
-
-  private:
-    struct Entry {
-        Job job;
-        void* data;
-    };
-
-    volatile Entry queue[JOB_QUEUE_LEN];
-    volatile u32 head;
-    volatile u32 tail;
-
-  public:
-    // called from only 1 thread
-    auto add(Job job, void* data) -> void {
-        auto i = head % JOB_QUEUE_LEN;
-        queue[i].job = job;
-        queue[i].data = data;
-        head = head + 1;
-    }
-
-    // called from multiple threads
-    // returns:
-    //   true if queue was not empty (even if compare and exchange failed)
-    //   false if queue was empty for sure
-    auto run_next() -> bool {
-        auto t = tail;
-        if (t == head) {
-            // is empty
-            return false;
-        }
-
-        if (atomic_compare_exchange(&tail, t, t + 1)) {
-            // got job
-            auto& job = queue[t % JOB_QUEUE_LEN];
-            job.job(job.data);
-        }
-
-        // some other thread got the job
-        // queue was not empty for sure
-        return true;
-    }
-};
-
-extern JobQueue job_queue;
 JobQueue job_queue; // note: 0 initialized
 
 [[noreturn]] auto run_core([[maybe_unused]] u32 core_id) -> void {
