8938f7b3c811e8841564fc56f2e80d3bd912929a
calint <calin.tenitchi@gmail.com>
2026-02-04 18:56:12 +0100

    .

--- run.sh
+++ run.sh
@@ -48,7 +48,7 @@ clang++ -target x86_64-unknown-windows-msvc \
     -o esp/EFI/BOOT/BOOTX64.EFI \
     uefi.o kernel_asm.o kernel.o osca.o
 
-qemu-system-x86_64 -m 16G -vga std -serial stdio \
+qemu-system-x86_64 -enable-kvm -m 16G -vga std -serial stdio \
     -smp 4,sockets=1,cores=2,threads=2 \
     -drive if=pflash,format=raw,readonly=on,file=/usr/share/OVMF/x64/OVMF_CODE.4m.fd \
     -drive format=raw,file=fat:rw:esp # -full-screen
