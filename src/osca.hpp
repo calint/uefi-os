040611ec23f7379e7760cca836ab4815caefd70f
calint <calin.tenitchi@gmail.com>
2026-02-05 13:40:18 +0100

    .

--- src/osca.hpp
+++ src/osca.hpp
@@ -70,8 +70,9 @@ template <u32 QueueSize = 256> class Jobs final {
     // iso c++ standard  ub      mixed-size overlapping atomics are undefined.
     // x86_64 hardware   defined hardware guarantees cache-line coherence
     // gcc/clang         safe    built-ins handle the aliasing correctly
-    static_assert(__x86_64__ && __clang__,
-                  "this implementation requires x86_64 and clang");
+#if !defined(__x86_64__) || !(defined(__GNUC__) || defined(__clang__))
+    static_assert(false, "this implementation requires x86_64 and gcc/clang");
+#endif
 
     // make sure `state_` is alone on cache line
     [[maybe_unused]] u8 padding[CACHE_LINE_SIZE - sizeof(state_)];
