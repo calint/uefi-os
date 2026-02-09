e927754f980d2bcda6ddf4a400ebcbf220c56e98
calint <calin.tenitchi@gmail.com>
2026-02-09 16:54:23 +0100

    .

--- src/config.hpp
+++ src/config.hpp
@@ -4,6 +4,6 @@ namespace config {
 
 auto constexpr CORE_STACK_SIZE_PAGES = 2 * 1024 * 1024 / 4096u;
 
-auto constexpr TIMER_FREQUENCY_HZ = 2;
+auto constexpr TIMER_FREQUENCY_HZ = 2u;
 
 }; // namespace config
